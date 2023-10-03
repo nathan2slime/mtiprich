a47151a477fb2e8df65ef829cc7002ef154ec7ef
Jhonathan <nathan3boss@gmail.com>
2023-10-03 18:55:40 -0300

    move fonts css

--- apps/playground/src/app/page.tsx
+++ apps/playground/src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { MtiEditor } from '@nathan3boss/mtiprich';
 
 import '@nathan3boss/mtiprich/dist/index.css';
+import '@nathan3boss/mtiprich/dist/src/global/fonts.css';
 
 const Index = () => {
   const [value, setValue] = useState('');
