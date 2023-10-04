6d17796c58bfe6ab885218f9e98cf29dc4eda24b
Jhonathan <nathan3boss@gmail.com>
2023-10-03 23:40:43 -0300

    fix(core): removing unused imports (#3)

--- apps/playground/src/app/page.tsx
+++ apps/playground/src/app/page.tsx
@@ -4,9 +4,11 @@ import { useState } from 'react';
 import { MtiEditor } from '@nathan3boss/mtiprich';
 
 import '@nathan3boss/mtiprich/dist/index.css';
+import '@nathan3boss/mtiprich/dist/src/global/fonts.css';
 
 const Index = () => {
   const [value, setValue] = useState('');
+  
   return (
     <div className="p-9">
       <MtiEditor
